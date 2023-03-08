7f6dd42920db65af3ab7afad77173667b8f6260c
Aditya Desai <115383821+aditya-desai-19@users.noreply.github.com>
2023-03-08 20:41:25 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    return(
+        <div className="App">
+            <Router>
+            {/*Navbar component is placed here because to render it on every page*/}
+            <Navbar />
+                <Routes>
+                    <Route path="/" />
+                    <Route path="/cart" />
+                </Routes>
+            </Router>
+        </div>
+    )
 }
 
 export default App;
